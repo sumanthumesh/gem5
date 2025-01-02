05d018650a8c43b1b9eb7204845b10545cecdb6d
Sumanth Umesh <sumanthu@umich.edu>
2025-01-02 17:36:54 -0500

    New config that can do checkpointing with simple memory model and atomic CPU while running actual queries with O3 and ramulator. Works well.

---
+++ configs/example/tutorial/multi_core_checkpoint.py
@@ -0,0 +1,182 @@
+import argparse
+import os
+import sys
+
+from cache import (
+    L1DCache,
+    L1ICache,
+    L2Cache,
+    L3Cache,
+)
+
+import m5
+from m5.objects import *
+
+parser = argparse.ArgumentParser(
+    description="Multicore O3 CPU with private l1,l2 and shared l3. Memory is handled by ramulator"
+)
+parser.add_argument(
+    "-c", "--command", help="Command to run enclosed in double quotes"
+)
+parser.add_argument(
+    "-n", "--num-cores", help="Number of cores to simulate", default="1"
+)
+parser.add_argument(
+    "--checkpoint-dir", help="Directory to store checkpoint", default=None
+)
+parser.add_argument(
+    "--restore-dir",
+    help="Directory from which to pick checkpoint",
+    default=None,
+)
+
+args = parser.parse_args()
+
+# Both checkpoint-dir and restore-dir shouldnt be specified at the same time
+assert not (
+    args.restore_dir and args.checkpoint_dir
+), "Cannot have both saving checkpoint and restoring it in same command"
+
+# Check if the checkpoint dir exists
+if args.restore_dir:
+    assert os.path.exists(
+        args.restore_dir
+    ), f"Cannot find checkpoint at {args.restore_dir} to restore"
+
+NUM_CORES: int = int(args.num_cores)
+cmd: str = args.command
+
+system = System()
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = "1GHz"
+system.clk_domain.voltage_domain = VoltageDomain()
+
+if args.checkpoint_dir:
+    system.mem_mode = "atomic"
+else:
+    system.mem_mode = "timing"
+system.mem_ranges = [AddrRange("8GB")]
+
+# Memory bus
+system.membus = SystemXBar()
+
+# Create CPUs
+if args.checkpoint_dir:
+    system.cpus = [AtomicSimpleCPU(cpu_id=i) for i in range(NUM_CORES)]
+else:
+    system.cpus = [DerivO3CPU(cpu_id=i) for i in range(NUM_CORES)]
+# system.cpus = [TimingSimpleCPU(cpu_id=i) for i in range(NUM_CORES)]
+
+system.l3cache = L3Cache(size="8MB", assoc=16)  # Shared L3 cache
+
+system.l2_to_l3bus = SystemXBar()
+
+for cpu in system.cpus:
+    # Interrupt stuff
+    cpu.createInterruptController()
+    cpu.interrupts[0].pio = system.membus.mem_side_ports
+    cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+    cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+    # Create L1 caches (private to each core)
+    cpu.icache = L1ICache(size="1kB", assoc=4)
+    cpu.dcache = L1DCache(size="32kB", assoc=4)
+
+    # Connect CPU to L1
+    cpu.icache_port = cpu.icache.cpu_side
+    cpu.dcache_port = cpu.dcache.cpu_side
+    # cpu.icache_port = system.membus.cpu_side_ports
+    # cpu.dcache_port = system.membus.cpu_side_ports
+
+    # Create L2
+    cpu.l2cache = L2Cache(size="256kB", assoc=8)
+
+    # Create bus from L1 to L2
+    cpu.l1_to_l2bus = L2XBar()
+
+    # Connect L1 to bus
+    cpu.icache.mem_side = cpu.l1_to_l2bus.cpu_side_ports
+    cpu.dcache.mem_side = cpu.l1_to_l2bus.cpu_side_ports
+
+    # Connect bus to L2
+    cpu.l2cache.cpu_side = cpu.l1_to_l2bus.mem_side_ports
+
+    # Connect L2 to shared L3
+    cpu.l2cache.mem_side = system.l2_to_l3bus.cpu_side_ports
+
+# Connect shared bus to L3
+system.l3cache.cpu_side = system.l2_to_l3bus.mem_side_ports
+
+# Connect L3 to system bus
+system.l3cache.mem_side = system.membus.cpu_side_ports
+
+
+if args.checkpoint_dir:
+    system.mem_ctrl = MemCtrl()
+    system.mem_ctrl.dram = DDR3_1600_8x8()
+    system.mem_ctrl.dram.range = system.mem_ranges[0]
+    system.mem_ctrl.port = system.membus.mem_side_ports
+else:
+    system.mem_ctrl = Ramulator2()
+    system.mem_ctrl.config_path = "/data1/sumanthu/gem5/ext/ramulator2/ramulator2/example_config.yaml"
+    system.mem_ctrl.range = system.mem_ranges[0]
+    system.mem_ctrl.port = system.membus.mem_side_ports
+
+# Connect system port
+system.system_port = system.membus.cpu_side_ports
+
+# binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+# binary = '/data1/sumanthu/Splash-3/codes/apps/ocean/contiguous_partitions/OCEAN'
+# binary = '/data1/sumanthu/Splash-3/codes/kernels/fft/FFT'
+# binary = '/data1/sumanthu/Splash-3/codes/apps/ocean/contiguous_partitions/OCEAN'
+# binary = '/data1/sumanthu/hyrise/build_release/hyriseConsole'
+binary = cmd.split(" ")[0]
+# for gem5 V21 and beyond
+system.workload = SEWorkload.init_compatible(binary)
+
+process = Process()
+# process.cmd = [binary,'/data1/sumanthu/hyrise/build_release/script.sql']
+# process.cmd = [binary,'-p2', '-n258']
+# process.cmd = [binary,'-p2', '-m16']
+process.cmd = cmd.split(" ")
+print(process.cmd)
+for cpu in system.cpus:
+    cpu.workload = process
+    cpu.createThreads()
+
+root = Root(full_system=False, system=system)
+
+
+# Explicitly trigger a checkpoint from the Python script
+if args.restore_dir:
+    m5.instantiate(args.restore_dir)
+else:
+    m5.instantiate()
+# root.checkpoint_at = 1000000  # This is the tick when the checkpoint will be taken
+# m5.checkpoint("/data1/sumanthu/gem5/m5out")
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+
+print(
+    f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}"
+)
+
+# If the simulation decides to exit due to checkpoint, then resume simulation afterwards
+if exit_event.getCause() == "checkpoint":
+    if args.checkpoint_dir:
+        # Perform the checkpoint
+        print(f"Attemting to write checkpint to {args.checkpoint_dir}")
+        m5.checkpoint(args.checkpoint_dir)
+
+    print("Resuming simulation")
+    exit_event = m5.simulate()
+
+    print(
+        "Exiting @ tick {} because {}".format(
+            m5.curTick(), exit_event.getCause()
+        )
+    )
+
+    print(exit_event.getCode())
