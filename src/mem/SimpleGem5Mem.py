d246094ce1bf3b063554b78aea0de84a0df71b62
Sumanth Umesh <sumanthu@umich.edu>
2025-01-07 22:58:59 -0500

    Forgot this file. It is part of the m5_add_mem_region implementation

--- src/mem/SimpleGem5Mem.py
+++ src/mem/SimpleGem5Mem.py
@@ -1,15 +1,18 @@
-from m5.SimObject import *
-from m5.params import *
 from m5.objects.AbstractMemory import *
+from m5.params import *
 from m5.proxy import Parent
+from m5.SimObject import *
+
 
 class SimpleGem5Mem(AbstractMemory):
-  type = "SimpleGem5Mem"
-  cxx_class = "gem5::memory::SimpleGem5Mem"
-  cxx_header = "mem/simple_gem5_mem.hh"
+    type = "SimpleGem5Mem"
+    cxx_class = "gem5::memory::SimpleGem5Mem"
+    cxx_header = "mem/simple_gem5_mem.hh"
 
-  port = ResponsePort("The port for receiving memory requests and sending responses")
-  tck = Param.Float("DRAM clock period in ns")
+    port = ResponsePort(
+        "The port for receiving memory requests and sending responses"
+    )
+    tck = Param.Float("DRAM clock period in ns")
+
 
 #   system = Param.System(Parent.any, "System this memory is part of")
-  
