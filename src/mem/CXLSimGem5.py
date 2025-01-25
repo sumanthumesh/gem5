d57ee2fe1fddd1a4260ee97b2a64ca351e0b8e0b
Sumanth Umesh <sumanthu@umich.edu>
2025-01-25 15:16:28 -0500

    Forgot to add this file before

---
+++ src/mem/CXLSimGem5.py
@@ -0,0 +1,20 @@
+from m5.objects.AbstractMemory import *
+from m5.params import *
+from m5.proxy import Parent
+from m5.SimObject import *
+
+
+class CXLSimGem5(AbstractMemory):
+    type = "CXLSimGem5"
+    cxx_class = "gem5::memory::CXLSimGem5"
+    cxx_header = "mem/cxlsim_gem5.hh"
+
+    port = ResponsePort(
+        "The port for receiving memory requests and sending responses"
+    )
+    tck = Param.Float("DRAM clock period in ns")
+    config_path = Param.String("Path to ramulator config file")
+    skip_cycle = Param.Bool("Whether to skip cycles or not")
+
+
+#   system = Param.System(Parent.any, "System this memory is part of")
